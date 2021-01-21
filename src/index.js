bd0ea8569e0d1d21f860b85d398e5e2e7e801dc8
MayurHpNotebook <001mayur94@gmail.com>
2021-01-22 04:44:21 +0530

    password hashed using bcrypt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // const frontendUrl = "http://localhost:3000";
-const mongodbUrl = "mongodb://localhost:27017/meditationCommunity";
+const mongodbUrl = "mongodb+srv://user:userpassword@meditation-community-ap.vgg6t.mongodb.net/meditationCommunity?retryWrites=true&w=majority";
+//"mongodb://localhost:27017/meditationCommunity";
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -12,31 +13,69 @@ const userSchema = mongoose.Schema({
 })
 const User = mongoose.model('User', userSchema); //model
 const db = mongoose.connection;
-const data1 = {
-    fullName: "mayur",
-    email: "mayur.com",
-    password: "check"
-}
 
-
-
+//code for hashing user password start
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//code for hashing user password ends
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
     // we're connected!
     // set routes which needs db here
     console.log("connected to mongodb database");
-    app.post('/sign-up', (req, res) => {
-        const newUser=new User(req.body);
-         newUser.save((err,user)=>{
-             if(err){
-                 console.error(err);
-             }else{
-                 console.log("saved");
-             }    
-         })
-        res.send({ status: 'ok' });
-        console.log(req.body);
+
+    app.post('/sign-up', async (req, res) => {
+        const userObj = req.body; //copied the sign-up data
+        
+        const userDoc = await User.findOne({ email: req.body.email }).exec();
+        //go further only if new email
+        if(userDoc==null){
+            //hashing the password
+            bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                // Store hash in your password DB.
+                userObj.password = hash;
+                //store it to database;
+                const newUser = new User(userObj);
+                newUser.save((err, user) => {
+                    if (err) {
+                        console.error(err);
+                        res.send({ status: 'failed' });
+                    } else {
+                        console.log("saved");
+                        res.send({ status: 'ok' });
+                    }
+                })
+                console.log(userObj);
+            });
+        }else{
+            res.send({
+                status:'failed',
+                error:'already registered'
+            });
+        }
+    })
+
+    app.post('/sign-in', async (req, res) => {
+
+        //console.log(req.body);
+        //finding document of user
+        const userDoc = await User.findOne({ email: req.body.email }).exec();
+        console.log(userDoc);
+        if (userDoc != null) {
+            //compare passwored
+            bcrypt.compare(req.body.password, userDoc.password)
+                .then((result) => {
+                    if (result) {
+                        res.send({ status: 'ok' });
+                    } else {
+                        res.send({ status: 'failed' });
+                    }
+                });
+        } else {
+            res.send({ status: 'failed' });
+        }
+
     })
 
 });
@@ -51,10 +90,7 @@ app.get('/', (req, res) => {
     res.send({ name: "mayur", fatherName: "machindra" });
 })
 
-app.post('/sign-in', (req, res) => {
-    res.send({ status: 'ok' });
-    console.log(req.body);
-})
+
 
 
 //process.env.PORT defines the port from heroku
