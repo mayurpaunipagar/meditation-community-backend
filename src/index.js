d722237e6f148d6cda77aa1fc56c7b6628a19336
MayurHpNotebook <001mayur94@gmail.com>
2021-01-22 02:53:10 +0530

    added feature: sign-up body received from nodejs server and saved to mongodb

--- src/index.js
+++ src/index.js
@@ -1,19 +1,63 @@
-const express=require('express');
-const app=express();
-const port=9999;
+// const frontendUrl = "http://localhost:3000";
+const mongodbUrl = "mongodb://localhost:27017/meditationCommunity";
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const userSchema = mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String
+})
+const User = mongoose.model('User', userSchema); //model
+const db = mongoose.connection;
+const data1 = {
+    fullName: "mayur",
+    email: "mayur.com",
+    password: "check"
+}
 
-app.get('/',(req,res)=>{
-    res.send("hello mayur is here to help you");
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+    // we're connected!
+    // set routes which needs db here
+    console.log("connected to mongodb database");
+    app.post('/sign-up', (req, res) => {
+        const newUser=new User(req.body);
+         newUser.save((err,user)=>{
+             if(err){
+                 console.error(err);
+             }else{
+                 console.log("saved");
+             }    
+         })
+        res.send({ status: 'ok' });
+        console.log(req.body);
+    })
+
+});
+
+//set routes here which doesn't need.
+
+
+const port = 9999;
+app.use(cors());//for handling cors
+app.use(express.json());//for handling post request bodies
+app.get('/', (req, res) => {
+    res.send({ name: "mayur", fatherName: "machindra" });
 })
 
-app.post('/sign-in',(req,res)=>{
- console.log(req.body);
+app.post('/sign-in', (req, res) => {
+    res.send({ status: 'ok' });
+    console.log(req.body);
 })
 
-app.post('/sign-up',(req,res)=>{
- console.log(req.body);
-})
 
-app.listen(process.env.PORT || port,()=>{
-    console.log("Meditation-community backend is listening on port: "+port);
+//process.env.PORT defines the port from heroku
+app.listen(process.env.PORT || port, () => {
+    console.log("Meditation-community backend is listening on port: " + port);
 })
